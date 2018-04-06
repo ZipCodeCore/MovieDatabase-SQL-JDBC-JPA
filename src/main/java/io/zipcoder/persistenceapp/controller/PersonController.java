2afa887f0c941e9a98ce397842828d2a7efb9446
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-06 17:18:58 -0400

    saving

--- src/main/java/io/zipcoder/persistenceapp/controller/PersonController.java
+++ src/main/java/io/zipcoder/persistenceapp/controller/PersonController.java
@@ -3,8 +3,11 @@ package io.zipcoder.persistenceapp.controller;
 import io.zipcoder.persistenceapp.domain.Person;
 import io.zipcoder.persistenceapp.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
@@ -15,6 +18,11 @@ public class PersonController {
     @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
     public Person getPersonById(@PathVariable int id){
         return service.getPersonById(id);
+    }
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    public List<Person> getAllPersons () {
+        return service.getAllPerson();
     }
 
 }
--- src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class PersonService {
 
@@ -21,6 +27,25 @@ public class PersonService {
         String sql = "SELECT * FROM PERSON WHERE ID = ?";
         Person person = (Person)jdbc.queryForObject(sql, new Object[] {personId}, new PersonRowMapper());
         return person;
+    }
+
+    public List<Person> getAllPerson () {
+        String sql = "SELECT * FROM PERSON";
+        List<Person> people = new ArrayList<>();
+       List<Map<String,Object>> rows = jdbc.queryForList(sql);
+       for(Map row : rows) {
+           Person person = new Person();
+           person.setId((int)row.get("ID"));
+           person.setFirstName((String)row.get("FIRST_NAME"));
+           person.setLastName((String)row.get("LAST_NAME"));
+           person.setMobile((String)row.get("MOBILE"));
+           person.setBirthday((Date)row.get("BIRTHDAY"));
+           person.setHomeId((short)row.get("HOME_ID"));
+           people.add(person);
+
+       }
+
+       return people;
     }
 
 
