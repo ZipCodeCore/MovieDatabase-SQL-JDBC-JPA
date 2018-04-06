0918c0a3faddf534548f10cdeb0c45e065333764
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-06 16:59:07 -0400

    saving, getbyid method works

--- src/main/java/io/zipcoder/persistenceapp/controller/PersonController.java
+++ src/main/java/io/zipcoder/persistenceapp/controller/PersonController.java
@@ -1,4 +1,20 @@
 package io.zipcoder.persistenceapp.controller;
 
+import io.zipcoder.persistenceapp.domain.Person;
+import io.zipcoder.persistenceapp.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PersonController {
+
+    @Autowired
+    PersonService service;
+
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
+    public Person getPersonById(@PathVariable int id){
+        return service.getPersonById(id);
+    }
+
 }
--- src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
@@ -1,4 +1,28 @@
 package io.zipcoder.persistenceapp.service;
 
+import io.zipcoder.persistenceapp.domain.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+//    public void addPerson(Person person){
+//
+//        jdbc.query('INSERT into PERSON values (')
+//
+//    }
+
+    public Person getPersonById (int personId) {
+        String sql = "SELECT * FROM PERSON WHERE ID = ?";
+        Person person = (Person)jdbc.queryForObject(sql, new Object[] {personId}, new PersonRowMapper());
+        return person;
+    }
+
+
+
 }
