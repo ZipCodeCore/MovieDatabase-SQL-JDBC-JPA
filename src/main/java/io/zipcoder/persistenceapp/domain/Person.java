e863824d93ec4eda5e14c022acb9145d1d4d3e07
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-08 12:42:28 -0400

    Person pojo

---
+++ src/main/java/io/zipcoder/persistenceapp/domain/Person.java
@@ -0,0 +1,62 @@
+package io.zipcoder.persistenceapp.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private long id;
+
+    @NotEmpty
+    @Column(name = "First Name")
+    private String firstName;
+
+    @NotEmpty
+    @Column(name = "Last Name")
+    private String lastName;
+
+    @NotEmpty
+    @Column(name = "Birthday")
+    private String bithday;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBithday() {
+        return bithday;
+    }
+
+    public void setBithday(String bithday) {
+        this.bithday = bithday;
+    }
+
+}
