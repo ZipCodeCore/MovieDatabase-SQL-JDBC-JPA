3e4d1fb7fbec7b1a78351a9b39dacc8da7b7e63e
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-08 12:06:05 -0400

    Repo and controller started, PersonService stubs

--- src/main/java/io/zipcoder/persistenceapp/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/PersonService.java
@@ -1,7 +1,49 @@
 package io.zipcoder.persistenceapp;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class PersonService {
 
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
+    public void addPerson() {
+
+    }
+
+    public void updateExistingPerson() {
+
+    }
+
+    public void removePerson() {
+
+    }
+
+    public void removeListOfPeople() {
+
+    }
+
+    public void findPeopleWithFirstName() {
+
+    }
+
+    public void findPeopleWithLastName() {
+
+    }
+
+    public void findPeopleWithBirthdate() {
+
+    }
+
+    public void findPersonWithId() {
+
+    }
+
+    public void generateMapLastNameList() {
+        // return map<lastName, List<firstName of people with that lastName>>
+    }
+
+    public void generateMapFirstNameOccurrences() {
+        // return map<firstName, int occurrences>
+    }
 
 }
---
+++ src/main/java/io/zipcoder/persistenceapp/repositories/PersonRepository.java
@@ -0,0 +1,6 @@
+package io.zipcoder.persistenceapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository{
+}
