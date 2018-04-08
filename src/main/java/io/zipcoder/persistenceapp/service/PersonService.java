12f8628a112c11a75cb6caa621a1b9bbc4826d56
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-08 15:25:46 -0400

    added to personService interface

--- src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/service/PersonService.java
@@ -1,12 +1,34 @@
 package io.zipcoder.persistenceapp.service;
 
+import io.zipcoder.persistenceapp.domain.Person;
 import org.h2.engine.Database;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import java.util.List;
 
-@Service
 public interface PersonService {
+    List<Person> getCarByMake(String make);
+
+    void deleteCar(int id);
+
+    void deleteCars(List<Person> cars);
+
+    Iterable<Person> getSalesPackages();
+
+    Iterable<Person> getSalesPackagesForYear(String year);
+
+    void addCar(Person person);
+
+//    POST /people -- create a person
+//    PUT /people/{id} -- update person with id. 404 error if that person doesn't exist yet
+//    GET /people/{id} -- get the person with the specified ID
+//    DELETE /people/{id} -- Delete the person with the specified ID
+//    GET /people -- get all people in the database
+//    GET /people/reverselookup/{mobileNumber} -- find all people with the specified mobile number
+//    GET /people/surname/{lastName} -- Find all people with a particular last name
+//    GET /people/surname -- Get the result of the surname report above
+//    GET /people/firstname/stats -- Get the report of first name frequencies
 
 }
