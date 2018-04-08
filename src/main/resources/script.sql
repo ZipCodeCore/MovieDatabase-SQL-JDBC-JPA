97a9c0e0b1851897dac296b078eab1804aca8b50
Amy Gill <amy.gill.34@gmail.com>
2018-04-07 22:10:50 -0400

    table stubbed out. joy.

---
+++ src/main/resources/script.sql
@@ -0,0 +1,12 @@
+INSERT INTO movies (Title, Runtime, Genre, IMDB_SCORE, Rating)
+Values ('Howard the Duck',110,'Sci-Fi',4.6,'PG'),('Starship Troopers',129,'Sci-Fi',7.2,'PG-13'),
+('Lavalantula', 83, 'Horror',4.7,'TV-14'), ('Waltz With Bashir',90, 'Documentary',8.0, 'R'),
+('Spaceballs',96, 'Comedy',7.1, 'PG'),
+('Monsters Inc.', 92, 'Animation',8.1, 'G');
+
+
+INSERT INTO movies (Title, Runtime, Genre, IMDB_SCORE, Rating)
+Values ('woeiuru',110,'Sci-Fi',4.6,'PG'),('dkgjie',129,'Sci-Fi',7.2,'PG-13'),
+('oiuogj', 83, 'Horror',4.7,'TV-14'), ('lskjig',90, 'Documentary',8.0, 'R'),
+('Shell',96, 'Comedy',7.1, 'PG'),
+('HEYYYYY MAX.', 92, 'Animation',8.1, 'G');
