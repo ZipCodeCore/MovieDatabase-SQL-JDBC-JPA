3c3d8d21c2bd948e3d79d7f7d99c7b299a0e27cd
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-08 20:54:48 -0400

    PersonService set up

--- src/main/java/io/zipcoder/persistenceapp/services/PersonService.java
+++ src/main/java/io/zipcoder/persistenceapp/services/PersonService.java
@@ -1,10 +1,17 @@
 package io.zipcoder.persistenceapp.services;
 
+import io.zipcoder.persistenceapp.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+    private PersonRepository personRepository;
 
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
 }
